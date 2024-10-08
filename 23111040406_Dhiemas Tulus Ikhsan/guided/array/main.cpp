79770b19900c9a9f860856e3957ab488798731e2
zclimaxsp <151652931+zclimaxsp@users.noreply.github.com>
2024-10-08 23:10:32 +0700

    Add files via upload

---
+++ 23111040406_Dhiemas Tulus Ikhsan/guided/array/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+     int nilai[5]={1,2,3,4,5};
+
+    // mencetak nilai array menggunakan pernyataan cout
+    cout << nilai[0];
+    cout << nilai[1];
+    cout << nilai[2];
+    cout << nilai[3];
+    cout << nilai[4];
+
+    // mencetak nilai array menggunakan loop for
+    for(int i=0; i<5; i++){
+        cout << nilai[i] << endl;
+    }
+
+    return 0;
+
+}
