dd34e72905e29558c2f108f11406686114fc5d8e
onyooooon <168392541+onyooooon@users.noreply.github.com>
2024-10-08 23:45:30 +0700

    Add files via upload

---
+++ 2311103047_BerlianSevaAstryana/Guided/guided/guided1.2/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <conio.h> // include conio.h for getch()
+
+using namespace std;
+
+int main() {
+    int nilai[5]={1,2,3,4,5};
+
+    // mencetak nilai array menggunakan pernyataan cout
+    /*cout << nilai[0];
+    cout << nilai[1];
+    cout << nilai[2];
+    cout << nilai[3];
+    cout << nilai[4];*/
+
+    // mencetak nilai array menggunakan loop for
+    for(int i=0; i<5; i++){
+        cout << nilai[i] << endl;
+    }
+
+    getch();
+    return 0;
+}
